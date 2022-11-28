a264d5ae13ba8c84d4a12f40b24993dcd21a8028
riga037 <ricardo.garciab5@gmail.com>
2022-11-28 13:19:22 +0100

    All Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         	TypeSeeder::class,
             MonsterSeeder::class,
             MoveSeeder::class,
+            MonsterMoveSeeder::class,
          ]);
     }
 }
