4c22a3df2ca3c63e1f694848cb21ac740b461d8c
xavivillenaviudez <95481962+xavivillenaviudez@users.noreply.github.com>
2022-11-28 13:05:38 +0100

    moves&moonsters

--- database/seeders/MonsterMoveSeeder.php
+++ database/seeders/MonsterMoveSeeder.php
@@ -26,11 +26,11 @@ class MonsterMoveSeeder extends Seeder
         ]);
         DB::table('monsters_moves')->insert([
             'monster_id' => 1,
-            'move_id' => 3        
+            'move_id' => 5        
         ]);
         DB::table('monsters_moves')->insert([
             'monster_id' => 2,
-            'move_id' => 4        
+            'move_id' => 2        
         ]);
         DB::table('monsters_moves')->insert([
             'monster_id' => 2,
@@ -42,11 +42,160 @@ class MonsterMoveSeeder extends Seeder
             'move_id' => 6        
         ]);
         DB::table('monsters_moves')->insert([
-            'monster_id' => 2,
+            'monster_id' => 3,
+            'move_id' => 3        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 3,
+            'move_id' => 4        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 3,
             'move_id' => 7        
         ]);
-        
-        
-
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 4,
+            'move_id' => 1        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 4,
+            'move_id' => 4        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 4,
+            'move_id' => 5        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 5,
+            'move_id' => 2        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 5,
+            'move_id' => 4        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 5,
+            'move_id' => 8        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 6,
+            'move_id' => 3        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 6,
+            'move_id' => 6        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 6,
+            'move_id' => 8        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 7,
+            'move_id' => 1        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 7,
+            'move_id' => 8        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 7,
+            'move_id' => 9        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 8,
+            'move_id' => 2        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 8,
+            'move_id' => 5        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 8,
+            'move_id' => 6        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 9,
+            'move_id' => 3        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 9,
+            'move_id' => 4        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 9,
+            'move_id' => 5        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 10,
+            'move_id' => 1        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 10,
+            'move_id' => 4        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 10,
+            'move_id' => 5        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 11,
+            'move_id' => 2        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 11,
+            'move_id' => 6        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 11,
+            'move_id' => 8        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 12,
+            'move_id' => 3        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 12,
+            'move_id' => 5        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 12,
+            'move_id' => 9        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 13,
+            'move_id' => 1        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 13,
+            'move_id' => 4        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 13,
+            'move_id' => 8        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 14,
+            'move_id' => 2        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 14,
+            'move_id' => 4        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 14,
+            'move_id' => 7        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 15,
+            'move_id' => 3        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 15,
+            'move_id' => 4        
+        ]);
+        DB::table('monsters_moves')->insert([
+            'monster_id' => 15,
+            'move_id' => 7        
+        ]);
     }
 }
