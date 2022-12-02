8e06e40b9d97ebd966cb806661ea17e6b992c195
xavivillenaviudez <95481962+xavivillenaviudez@users.noreply.github.com>
2022-12-02 10:34:43 +0100

    v

--- storage/framework/views/b86254142b483ed04d81a8be8b7330821740ce45.php
+++ storage/framework/views/b86254142b483ed04d81a8be8b7330821740ce45.php
@@ -24,6 +24,7 @@
         <div class="container">
             <a class="navbar-brand" href="<?php echo e(url('/')); ?>">
                 
+                
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="<?php echo e(__('Toggle navigation')); ?>">
                 <span class="navbar-toggler-icon"></span>
